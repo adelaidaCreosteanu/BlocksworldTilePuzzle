b81a6a101e5147942c129947442da5edca3c201d
Adelaida Creosteanu <adelaida.creosteanu@gmail.com>
2017-10-24 12:08:32 +0100

    Outline of Board constructor

--- Board.java
+++ Board.java
@@ -1,3 +1,27 @@
 public class Board {
+    private int size;
+    private int[][] state;
+    private int agentx;
+    private int agenty;
 
+    public Board (int size) {
+        this.size = size;
+        state = new int[size][size];
+
+        // Put in zeros
+        for (int i = 0; i < size-1; i ++) {
+            for (int j = 0; j < size; j ++) {
+                state[i][j] = 0;
+            }
+        }
+
+        // Put in letter blocks
+        for (int j = 0; j < size-1; j ++) {
+            state[size-1][j] = 65+1;
+        }
+
+        // Put in the agent in the bottom-right corner
+        agentx = size-1;
+        agenty = size-1;
+    }
 }
